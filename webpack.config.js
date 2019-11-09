03b5f20cece4dbea5331bc994be8e33f4a470e1d
nagyl <nagylaszlo9423@gmail.com>
2019-11-09 20:03:39 +0100

    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
 'use strict';
 
-var nodeExternals = require('webpack-node-externals');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: {
+    index: './src/index.ts'
+  },
   mode: "production",
   output: {
-    filename: 'index.js', // <-- Important
-    libraryTarget: 'this' // <-- Important
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'tyr-map',
+    umdNamedDefine: true
   },
-  target: 'node', // <-- Important
+  devtool: 'source-map',
+  target: 'node',
   module: {
     rules: [
       {
-        test: /\.ts?$/,
-        loader: 'ts-loader?'
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
       }
     ]
   },
   resolve: {
     extensions: [ '.ts', '.tsx', '.js' ]
   },
-  externals: [nodeExternals()] // <-- Important
+  externals: [nodeExternals()],
+  optimization: {
+    minimize: true
+  }
 };
